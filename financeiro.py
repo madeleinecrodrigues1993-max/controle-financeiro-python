9c482b866addaa71d34c1bc5af9c7b67dcbe4935
Madeleine Rodrigues <madeleinecrodrigues1993@gmail.com>
2026-06-26 17:01:43 -0300

    Implementa persistência com JSON

--- financeiro.py
+++ financeiro.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 from transacao import Transacao
 
 
@@ -5,6 +8,47 @@ class Financeiro:
 
     def __init__(self):
         self.transacoes = []
+        self.arquivo = "dados/movimentacoes.json"
+
+        self.carregar_dados()
+
+    def carregar_dados(self):
+
+        if not os.path.exists(self.arquivo):
+            return
+
+        with open(self.arquivo, "r", encoding="utf-8") as arquivo:
+
+            dados = json.load(arquivo)
+
+            for item in dados:
+
+                transacao = Transacao(
+                    item["descricao"],
+                    item["valor"],
+                    item["categoria"],
+                    item["tipo"]
+                )
+
+                transacao.data = item["data"]
+
+                self.transacoes.append(transacao)
+
+    def salvar_dados(self):
+
+        dados = []
+
+        for transacao in self.transacoes:
+            dados.append(transacao.to_dict())
+
+        with open(self.arquivo, "w", encoding="utf-8") as arquivo:
+
+            json.dump(
+                dados,
+                arquivo,
+                indent=4,
+                ensure_ascii=False
+            )
 
     def adicionar_transacao(self, descricao, valor, categoria, tipo):
 
@@ -16,6 +60,7 @@ class Financeiro:
         )
 
         self.transacoes.append(transacao)
+        self.salvar_dados()
 
     def listar_transacoes(self):
         return self.transacoes
